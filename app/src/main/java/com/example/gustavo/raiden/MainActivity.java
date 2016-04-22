8ee7171c01e5b60c8e35c597ed363956bb164555
BlueDi <diogohmcruz@hotmail.com>
2016-04-22 17:23:57 +0100

    test1

--- app/src/main/java/com/example/gustavo/raiden/MainActivity.java
+++ app/src/main/java/com/example/gustavo/raiden/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.gustavo.raiden;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    /** Called when the activity is first created. */
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // requesting to turn the title OFF
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // making it full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // set our MainGamePanel as the View
+        setContentView(new MainGamePanel(this));
+        Log.d(TAG, "View added");
     }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "Destroying...");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopping...");
+        super.onStop();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/gustavo/raiden/Ship.java
@@ -0,0 +1,7 @@
+package com.example.gustavo.raiden;
+
+/**
+ * Created by up201105483 on 22-04-2016.
+ */
+public class Ship {
+}
