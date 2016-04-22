f5ab4067f52905e778c4f08cd21e3bf0fc75f9b2
BlueDi <diogohmcruz@hotmail.com>
2016-04-22 18:18:48 +0100

    test3

--- app/src/main/java/com/example/gustavo/raiden/MainGamePanel.java
+++ app/src/main/java/com/example/gustavo/raiden/MainGamePanel.java
@@ -1,15 +1,18 @@
+/**
+ * 
+ */
 package com.example.gustavo.raiden;
 
-/**
- * Created by up201105483 on 22-04-2016.
- */
-        import android.app.Activity;
-        import android.content.Context;
-        import android.graphics.Canvas;
-        import android.util.Log;
-        import android.view.MotionEvent;
-        import android.view.SurfaceHolder;
-        import android.view.SurfaceView;
+import com.example.gustavo.raiden.model.Droid;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * @author impaler
@@ -17,69 +20,92 @@ package com.example.gustavo.raiden;
  * the image to the screen.
  */
 public class MainGamePanel extends SurfaceView implements
-        SurfaceHolder.Callback {
+		SurfaceHolder.Callback {
 
-    private static final String TAG = MainGamePanel.class.getSimpleName();
+	private static final String TAG = MainGamePanel.class.getSimpleName();
+	
+	private MainThread thread;
+	private Droid droid;
 
-    private MainThread thread;
+	public MainGamePanel(Context context) {
+		super(context);
+		// adding the callback (this) to the surface holder to intercept events
+		getHolder().addCallback(this);
 
-    public MainGamePanel(Context context) {
-        super(context);
-        // adding the callback (this) to the surface holder to intercept events
-        getHolder().addCallback(this);
+		// create droid and load bitmap
+		droid = new Droid(BitmapFactory.decodeResource(getResources(), R.drawable.hld), 50, 50);
+		
+		// create the game loop thread
+		thread = new MainThread(getHolder(), this);
+		
+		// make the GamePanel focusable so it can handle events
+		setFocusable(true);
+	}
 
-        // create the game loop thread
-        thread = new MainThread(getHolder(), this);
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
 
-        // make the GamePanel focusable so it can handle events
-        setFocusable(true);
-    }
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// at this point the surface is created and
+		// we can safely start the game loop
+		thread.setRunning(true);
+		thread.start();
+	}
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                               int height) {
-    }
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d(TAG, "Surface is being destroyed");
+		// tell the thread to shut down and wait for it to finish
+		// this is a clean shutdown
+		boolean retry = true;
+		while (retry) {
+			try {
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				// try again shutting down the thread
+			}
+		}
+		Log.d(TAG, "Thread was shut down cleanly");
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			// delegating event handling to the droid
+			droid.handleActionDown((int)event.getX(), (int)event.getY());
+			
+			// check if in the lower part of the screen we exit
+			if (event.getY() > getHeight() - 50) {
+				thread.setRunning(false);
+				((Activity)getContext()).finish();
+			} else {
+				Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
+			}
+		} if (event.getAction() == MotionEvent.ACTION_MOVE) {
+			// the gestures
+			if (droid.isTouched()) {
+				// the droid was picked up and is being dragged
+				droid.setX((int)event.getX());
+				droid.setY((int)event.getY());
+			}
+		} if (event.getAction() == MotionEvent.ACTION_UP) {
+			// touch was released
+			if (droid.isTouched()) {
+				droid.setTouched(false);
+			}
+		}
+		return true;
+	}
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        // at this point the surface is created and
-        // we can safely start the game loop
-        thread.setRunning(true);
-        thread.start();
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.d(TAG, "Surface is being destroyed");
-        // tell the thread to shut down and wait for it to finish
-        // this is a clean shutdown
-        boolean retry = true;
-        while (retry) {
-            try {
-                thread.join();
-                retry = false;
-            } catch (InterruptedException e) {
-                // try again shutting down the thread
-            }
-        }
-        Log.d(TAG, "Thread was shut down cleanly");
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            if (event.getY() > getHeight() - 50) {
-                thread.setRunning(false);
-                ((Activity)getContext()).finish();
-            } else {
-                Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
-            }
-        }
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-    }
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// fills the canvas with black
+		canvas.drawColor(Color.BLACK);
+		droid.draw(canvas);
+	}
 
 }
