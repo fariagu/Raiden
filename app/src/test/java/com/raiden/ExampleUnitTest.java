fddb0aac73958e40de7d24c2ba82d63be442f1e4
fariagu <gustavoduartefaria@gmail.com>
2016-06-06 16:09:39 +0100

    first attempt at testing

--- app/src/test/java/com/raiden/ExampleUnitTest.java
+++ app/src/test/java/com/raiden/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package com.raiden;
 
+import android.graphics.Bitmap;
+import android.util.DisplayMetrics;
+
+import com.raiden.logic.*;
+
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,7 +16,14 @@ import static org.junit.Assert.assertEquals;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void ShipMoves() throws Exception {
+        DisplayMetrics d = new DisplayMetrics();
+        int[] colors = new int[20*20];
+        Arrays.fill(colors, 0);
+        Bitmap b = Bitmap.createBitmap(d, colors, 20, 20, Bitmap.Config.RGB_565);
+        Ship s = new Ship(b, 10, 10, 0);
+
+        assertEquals(10, s.getX());
+        assertEquals(10, s.getY());
     }
 }
